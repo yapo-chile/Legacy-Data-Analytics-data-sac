a2b01ea1a8726650d3739ed2be7ed1a7fcb52145
Fernando Palomera <fernando.palomera@adevinta.com>
2021-08-25 13:14:08 -0400

    fix blocket config

--- implio-report/app/infraestructure/conf.py
+++ implio-report/app/infraestructure/conf.py
@@ -11,18 +11,18 @@ class AppConfig:
     AppConfig Class representing the configuration of the application
     """
 
-    @environ.config(prefix="DB")
-    class DBConfig:
+    @environ.config(prefix="BLOCKET")
+    class BlocketConfig:
         """
-        DBConfig Class representing the configuration to access the database
+        DBConfig Class representing the configuration to access the Blocket database
         """
-        host: str = INI_DB.secret(name="host", default=environ.var())
-        port: int = INI_DB.secret(name="port", default=environ.var())
-        name: str = INI_DB.secret(name="dbname", default=environ.var())
-        user: str = INI_DB.secret(name="user", default=environ.var())
-        password: str = INI_DB.secret(name="password", default=environ.var())
+        host: str = INI_BLOCKET.secret(name="host", default=environ.var())
+        port: int = INI_BLOCKET.secret(name="port", default=environ.var())
+        name: str = INI_BLOCKET.secret(name="dbname", default=environ.var())
+        user: str = INI_BLOCKET.secret(name="user", default=environ.var())
+        password: str = INI_BLOCKET.secret(name="password", default=environ.var())
 
-    db = environ.group(DBConfig)
+    blocket = environ.group(BlocketConfig)
 
 
 def getConf():
--- implio-report/app/usecases/implio_report.py
+++ implio-report/app/usecases/implio_report.py
@@ -12,7 +12,7 @@ class ImplioReport(ImplioReportQuery):
 
     def get_data(self) -> type[DataFrame]:
 
-        db_source = DataBase(conf=self.config)
+        db_source = DataBase(conf=self.config.blocket)
         df_data = db_source.select_to_dict(query=self.query_implio_report())
         db_source.close_connection()
 
@@ -22,7 +22,7 @@ class ImplioReport(ImplioReportQuery):
 
         df_data = self.get_data()
         self.logger(f'Data retrieved successfully. Number of rows: {df_data.shape[0]}')
-        df_data['month_id'] = df_data['action_timestamp'].map(lambda x: str(x)[0:7])
+        df_data.loc[:, 'month_id'] = df_data['action_timestamp'].map(lambda x: str(x)[0:7])
         year_month = self.params.get_last_month()
 
         return df_data[df_data['month_id'] == f'{year_month}']
