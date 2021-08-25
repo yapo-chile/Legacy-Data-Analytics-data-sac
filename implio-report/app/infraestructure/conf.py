f21f063aa36b2977089908193195ffd5480b5f9a
Fernando Palomera <fernando.palomera@adevinta.com>
2021-08-25 12:37:18 -0400

    rm unused files and conf

--- implio-report/app/infraestructure/conf.py
+++ implio-report/app/infraestructure/conf.py
@@ -11,23 +11,6 @@ class AppConfig:
     AppConfig Class representing the configuration of the application
     """
 
-    @environ.config(prefix="PULSE")
-    class AthenaConfig:
-        """
-        AthenaConfig class represeting the configuration to access
-        pulse service
-        """
-        s3_bucket: str = INI_PULSE.secret(
-            name="bucket", default=environ.var())
-        user: str = INI_PULSE.secret(
-            name="user", default=environ.var())
-        access_key: str = INI_PULSE.secret(
-            name="accesskey", default=environ.var())
-        secret_key: str = INI_PULSE.secret(
-            name="secretkey", default=environ.var())
-        region: str = INI_PULSE.secret(
-            name="region", default=environ.var())
-
     @environ.config(prefix="DB")
     class DBConfig:
         """
@@ -38,7 +21,7 @@ class AppConfig:
         name: str = INI_DB.secret(name="dbname", default=environ.var())
         user: str = INI_DB.secret(name="user", default=environ.var())
         password: str = INI_DB.secret(name="password", default=environ.var())
-    athenaConf = environ.group(AthenaConfig)
+
     db = environ.group(DBConfig)
 
 
