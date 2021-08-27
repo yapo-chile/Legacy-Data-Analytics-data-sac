e546fc1e7eafc6d41d2145f84ec398c60e8b8187
Fernando Palomera <fernando.palomera@adevinta.com>
2021-08-27 16:38:42 -0400

    TEST 2

--- implio-report/app/usecases/process.py
+++ implio-report/app/usecases/process.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-member
 # utf-8
+import pandas as pd  # FIXME: TEST
 from __future__ import annotations
 from usecases.implio_report import ImplioReport
 from usecases.output_handler import OutputHandler
@@ -19,10 +20,11 @@ class Process:
     def generate(self) -> None:
 
         self.logger.info('The process begins')
-        df_report = ImplioReport(logger=self.logger,
-                                 config=self.config,
-                                 params=self.params)\
-            .generate()
+        #df_report = ImplioReport(logger=self.logger,
+        #                         config=self.config,
+        #                         params=self.params)\
+        #    .generate() # FIXME: TEST
+        df_report = pd.read_csv('implio_null_revisions_202107.csv')  # FIXME: TEST
         self.logger.info(f'Report generated successfully, found {df_report.shape[0]} records')
         OutputHandler(logger=self.logger,
                       params=self.params)\
